38c6797eee7cc28394ebad7799bf81fc759fd3bb
HaoningLan <546004464@qq.com>
2021-02-28 21:33:37 +0800

    finish homework2

---
+++ Homework-Chapter-2/MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,27 @@
+
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btn).setOnClickListener(v -> {
+
+            Intent intent = new Intent(MainActivity.this, MyActivity.class);
+            intent.putExtra("userId", 123456);
+            startActivity(intent);
+        });
+
+    }
+
+}
